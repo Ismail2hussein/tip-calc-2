3b46e19d74d84590ac16dff178ff21464c8e5c4e
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-05-25 16:53:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculateTip(bill) {
     let tip;
-    if (bill >= 50 && bill <= 300) {
+    if (bill >= 80 && bill <= 300) {
       tip = bill * 0.15;
     } else {
       tip = bill * 0.2;
@@ -9,7 +9,7 @@ function calculateTip(bill) {
   }
   
   const bill1 = 100;
-  const bill2 = 500;
+  const bill2 = 300;
   const tip1 = calculateTip(bill1);
   const tip2 = calculateTip(bill2);
   console.log(`For bill of ${bill1}, the tip is ${tip1}`);
